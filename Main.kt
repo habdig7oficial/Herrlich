9e5ce03b5b31fb017977ccca4923cdaf9d795653
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-09-15 15:09:10 -0300

    linked

--- Main.kt
+++ Main.kt
@@ -1,25 +1,56 @@
+class Node <Generic> (
+    element: Generic,
+    next: Node<Generic>?
+){
+    var element: Generic = element
+    var next: Node<Generic>? = next
+}
+
+
+class LinkedList <Generic> {
+
+    var root: Node<Generic>? = null
+    var length: Int = 0
+    
+    fun append(element: Generic){
+        root = Node<Generic>(element, root)
+        this.length++
+    }
+    
+    fun print(){
+        var e: Node<Generic>? = root
+        for(i in 0..< this.length){
+            requireNotNull(e)
+            println(e.element)
+            e = e.next
+        }
+    }
+}
+
+
 open class Interpreter{
     val reserved: Array<String> = arrayOf("VARS", "RESET", "REC", "STOP", "PLAY", "ERASE","EXIT")
     val reservedSymbols: Array<Char> = arrayOf('+', '-', '*', '/', 'ˆ', '%', '(', ')', '=')
 
-    fun tokenize(textStream : String) : MutableList<String> {
+    fun tokenize(textStream : String) : LinkedList<String> {
 
-        val tokens: MutableList<String> = mutableListOf("") 
+        val tokens: LinkedList<String> = LinkedList()
+
 
         var i : Int = 0
         var ptr : Int = 0
 
         while(i < textStream.length){
             if(textStream[i] in reservedSymbols){
-                tokens.add(textStream[i].toString())
+                tokens.append(textStream[i].toString())
             }
             else if(textStream[i].isLetterOrDigit() || textStream[i] == '.'){
                 //tokens[tokens.size - 2] = tokens[ptr] + textStream[i]
                 //print("${textStream[i]}")
-                tokens.add(textStream[i].toString())
+                tokens.append(textStream[i].toString())
             }
-            else if(textStream[i].isWhitespace() && tokens[if(ptr == 0) 0 else(ptr - 1)] != ""){
-                tokens.add("")
+            else if(textStream[i].isWhitespace()){
+                tokens.append("")
             }
             i++
         }
@@ -35,7 +66,8 @@ class Repl : Interpreter() {
         while(true) {
             print("> ")
             val str = tokenize(readln())
-            print(str.joinToString())
+            //print(str.length)
+            str.print()
         }
     }
 }
